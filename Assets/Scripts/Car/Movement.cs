28fb906c9c1c54104bcc860042c0dde0a718f515
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-04-25 23:56:35 +0300

    Added movement basic script. Will continue tomorrow, tired a little bit.

---
+++ Assets/Scripts/Car/Movement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Car
+{
+    public class Movement : MonoBehaviour
+    {
+        private float speed = 5.0f;
+
+        private void Update()
+        {
+            transform.Translate(Vector2.up * speed * Time.deltaTime);
+        }
+    }
+}
