50a72410abc39bcdae14ed497bdb347bc1f65e26
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-04-26 19:31:09 +0300

    Trying to add car stopping before another car. Still have few bugs.

--- Assets/Scripts/Car/Movement.cs
+++ Assets/Scripts/Car/Movement.cs
@@ -9,8 +9,10 @@ namespace Car
         private float currentSpeed;
         private Characteristics characteristics;
         private BoxCollider fieldOfView;
-        private Movement carInfront = null;
-        private TrafficLightController lightController = null;
+        public Movement carInfront = null;
+        public TrafficLightController lightController = null;
+        private float stopSpeed = 0.0f;
+        private float distance = 0.0f;
 
         public float CurrentSpeed { get { return currentSpeed; } }
 
@@ -24,25 +26,50 @@ namespace Car
 
         private void Update()
         {
+            Move();
             if ((lightController == null || lightController.IsGreen) && (carInfront == null || carInfront.CurrentSpeed > currentSpeed))
             {
-                transform.Translate(characteristics.Direction * currentSpeed * Time.deltaTime);
+                currentSpeed = characteristics.MaxSpeed;
             }
             else
             {
-
+                if (currentSpeed > 0.0f)
+                {
+                    currentSpeed -= stopSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    currentSpeed = 0.0f;
+                }
             }
+        }
+
+        private void Move()
+        {
+            transform.Translate(characteristics.Direction * currentSpeed * Time.deltaTime);
+        }
+
+        private float CalculateAcceleration(float distance)
+        {
+            return distance < 0.0f ? -1.0f : 2 * currentSpeed / distance;
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            if(other.CompareTag("Traffic light"))
+            if (other.CompareTag("Traffic light"))
             {
                 lightController = other.gameObject.GetComponent<TrafficLightController>();
+                distance = CalculateDistance();
+                stopSpeed = CalculateAcceleration(distance);
             }
-            else
+            else if (other.CompareTag("Car") && carInfront == null)
             {
-                carInfront = other.gameObject.GetComponent<Movement>();
+                if(Vector2.Angle(transform.position - other.transform.position, characteristics.Direction) == 180.0f)
+                {
+                    carInfront = other.gameObject.GetComponent<Movement>();
+                    distance = CalculateDistance();
+                    stopSpeed = CalculateAcceleration(distance);
+                }
             }
         }
 
@@ -52,10 +79,27 @@ namespace Car
             {
                 lightController = null;
             }
-            else
+            else if (other.CompareTag("Car"))
             {
                 carInfront = null;
             }
+        }
+
+        private float CalculateDistance()
+        {
+            var distanceToLight = -1.0f;
+            var distanceToCar = -1.0f;
+
+            if(lightController != null)
+            {
+                distanceToLight = (lightController.transform.position - transform.position).magnitude;
+            }
+            if(carInfront != null)
+            {
+                distanceToCar = (carInfront.transform.position - transform.position).magnitude;
+            }
+
+            return distanceToLight > distanceToCar ? distanceToLight : distanceToCar;
         }
     }
 }
