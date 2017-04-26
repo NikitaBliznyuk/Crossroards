d6cbb8576dda0dd3a9a4cffabff82e5218df63df
Nikita Bliznyuk <nikbleid96@gmail.com>
2017-04-26 15:00:18 +0300

    Added crossroad controller.

---
+++ Assets/Scripts/Controllers/CrossroadsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossroadsController : MonoBehaviour
+{
+    [Range(0.0f, 10.0f)]
+    public float lightTime;
+
+    public TrafficLightController[] verticalLights;
+    public TrafficLightController[] horizontalLights;
+
+    private float currentTime = 0.0f;
+
+    private void Update()
+    {
+        for (var i = 0; i < horizontalLights.Length; i++)
+        {
+            horizontalLights[i].ToggleState(currentTime < lightTime);
+            verticalLights[i].ToggleState(currentTime >= lightTime);
+        }
+
+        currentTime = currentTime >= 10.0f ? 0.0f : currentTime + Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Controllers/TrafficLightController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLightController : MonoBehaviour
+{
+    private enum Lights { Red, Yellow, Green }
+
+    public List<Sprite> colors;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Lights state = Lights.Red;
+
+    public bool IsGreen { get { return state == Lights.Green; } }
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void ToggleState(bool on)
+    {
+        state = on ? Lights.Green : Lights.Red;
+
+        spriteRenderer.sprite = colors[(int)state];
+    }
+}
